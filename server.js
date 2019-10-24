5c1af8a493443a5718959eb7ba13072f01508232
hamdisaqaf <54242485+hamdialsakkaf@users.noreply.github.com>
2019-10-25 01:58:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -40,6 +40,46 @@ app.get('/update',function(req,res){
   
   console.log(' update database')
 });
+function check_alive(){
+   const myImage = document.querySelector('.my-image');
+fetch('https://upload.wikimedia.org/wikipedia/commons/7/77/Delete_key1.jpg')
+	.then(res => res.blob())
+	.then(res => {
+		const objectURL = URL.createObjectURL(res);
+		myImage.src = objectURL;
+});
+}
+
+self.addEventListener('sync', function (event) {
+    if(event.tag == 'myFirstSyns') {
+        event.waitUntil(check_alive());
+    }
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('Handling fetch event for', event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        console.log('Found response in cache:', response);
+
+        return response;
+      }
+      console.log('No response found in cache. About to fetch from network...');
+
+      return fetch(event.request).then(function(response) {
+        console.log('Response from network is:', response);
+
+        return response;
+      }).catch(function(error) {
+        console.error('Fetching failed:', error);
+
+        throw error;
+      });
+    })
+  );
+});
 
 app.listen(3000,function(){
 
