384dacd5fb5211f7f2bfcf386bb49fabcc073f14
hamdisaqaf <54242485+hamdialsakkaf@users.noreply.github.com>
2019-10-25 01:52:29 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,40 @@
 function check_alive(){
-    var a;
-    $.ajax({
-        url: '/ping',
-        type: 'GET',
-        async: false,
-        success: function(data){
-            //Ready for MySQL insertion.
-            console.log("MySQL is UP");
-            a=0;
-        },
-        error: function(data) {
-            //Go in the indexDB
-            a=1;
-        }
-    });
-    return a;
+   const myImage = document.querySelector('.my-image');
+fetch('https://upload.wikimedia.org/wikipedia/commons/7/77/Delete_key1.jpg')
+	.then(res => res.blob())
+	.then(res => {
+		const objectURL = URL.createObjectURL(res);
+		myImage.src = objectURL;
+});
 }
 
 self.addEventListener('sync', function (event) {
     if(event.tag == 'myFirstSyns') {
         event.waitUntil(check_alive());
     }
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('Handling fetch event for', event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        console.log('Found response in cache:', response);
+
+        return response;
+      }
+      console.log('No response found in cache. About to fetch from network...');
+
+      return fetch(event.request).then(function(response) {
+        console.log('Response from network is:', response);
+
+        return response;
+      }).catch(function(error) {
+        console.error('Fetching failed:', error);
+
+        throw error;
+      });
+    })
+  );
 });
