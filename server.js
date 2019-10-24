0862bcc030b1e4fa727925670a68fa3130cf6253
hamdisaqaf <54242485+hamdialsakkaf@users.noreply.github.com>
2019-10-25 02:02:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,19 @@ app.set('views',__dirname + '/view');
 app.use(express.static(__dirname + '/js'));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
+ if ('serviceWorker' in navigator && 'SyncManager' in window) {
+          navigator.serviceWorker.register('/sw.js');
+          navigator.serviceWorker.ready.then(function (swRegistration) {
+              return swRegistration.sync.register('myFirstSyns');
+          }).catch(function (reason) {
+            //  postDataFromThePage();
+              alert('service worker dont work because OS restriction')
+
+          });
+      } else {
+          // postDataFromThePage();
+          alert('service worker dont support')
+      }
 
 
 app.get('/',function(req,res){
@@ -39,46 +52,6 @@ app.get('/ping',function(req,res){
 app.get('/update',function(req,res){
   
   console.log(' update database')
-});
-function check_alive(){
-   const myImage = document.querySelector('.my-image');
-fetch('https://upload.wikimedia.org/wikipedia/commons/7/77/Delete_key1.jpg')
-	.then(res => res.blob())
-	.then(res => {
-		const objectURL = URL.createObjectURL(res);
-		myImage.src = objectURL;
-});
-}
-
-self.addEventListener('sync', function (event) {
-    if(event.tag == 'myFirstSyns') {
-        event.waitUntil(check_alive());
-    }
-});
-
-self.addEventListener('fetch', function(event) {
-  console.log('Handling fetch event for', event.request.url);
-
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (response) {
-        console.log('Found response in cache:', response);
-
-        return response;
-      }
-      console.log('No response found in cache. About to fetch from network...');
-
-      return fetch(event.request).then(function(response) {
-        console.log('Response from network is:', response);
-
-        return response;
-      }).catch(function(error) {
-        console.error('Fetching failed:', error);
-
-        throw error;
-      });
-    })
-  );
 });
 
 app.listen(3000,function(){
