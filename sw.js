27865b0ea799d1ffa78da9a009cc54c16f551ccc
hamdisaqaf <54242485+hamdialsakkaf@users.noreply.github.com>
2019-10-25 02:06:48 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,5 @@
 function check_alive(){
-   const myImage = document.querySelector('.my-image');
-fetch('https://upload.wikimedia.org/wikipedia/commons/7/77/Delete_key1.jpg')
-	.then(res => res.blob())
-	.then(res => {
-		const objectURL = URL.createObjectURL(res);
-		myImage.src = objectURL;
-});
+console.log('check_alive');
 }
 
 self.addEventListener('sync', function (event) {
@@ -14,9 +8,17 @@ self.addEventListener('sync', function (event) {
     }
 });
 
+
+
 self.addEventListener('fetch', function(event) {
   console.log('Handling fetch event for', event.request.url);
-
+  const myImage = document.querySelector('.my-image');
+fetch('https://upload.wikimedia.org/wikipedia/commons/7/77/Delete_key1.jpg')
+	.then(res => res.blob())
+	.then(res => {
+		const objectURL = URL.createObjectURL(res);
+		myImage.src = objectURL;
+});
   event.respondWith(
     caches.match(event.request).then(function(response) {
       if (response) {
